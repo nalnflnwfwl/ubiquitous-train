99edf1ec2042587d0cc6ddf6e7fecb58b21079e8
nalnflnwfwl <belindasim294@gmail.com>
2025-08-31 15:28:31 +0800

    add: init

---
+++ test_app.py
@@ -0,0 +1,10 @@
+# test_app.py
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1,2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
